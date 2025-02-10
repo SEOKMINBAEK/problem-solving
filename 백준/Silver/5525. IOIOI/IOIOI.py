18be4535b7231b35c51f0424477eee2a3193ac9a
백석민 <93717786+SEOKMINBAEK@users.noreply.github.com>
2025-02-10 11:12:11 +0900

    [Silver I] Title: IOIOI, Time: 320 ms, Memory: 34368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -4,11 +4,21 @@ input = sys.stdin.readline
 N = int(input())
 M = int(input())
 S = input().rstrip()
-P = 'I' + 'OI' * N
-cnt = 0
 
-for i in range(M - len(P) + 1):
-  if S[i] == 'I' and S[i:i + len(P)] == P:
-    cnt += 1
+i = 0
+cnt = 0 # P(n)이 발견
+oi_cnt = 0 # OI가 연속된 횟수
+
+while i < M - 1:
+  if S[i:i + 3] == 'IOI': # IOI 패턴이 발견되면
+    oi_cnt += 1 # P(1)로써 카운트 시작
+    i += 2 # 2칸 점프
+    
+    if oi_cnt == N: # P(n)이 완성된경우
+      cnt += 1 # 카운트
+      oi_cnt -= 1 # 좌측 OI 제거
+  else:
+    oi_cnt = 0 # 패턴이 끊기면 OI카운트 초기화
+    i += 1
 
 print(cnt)
